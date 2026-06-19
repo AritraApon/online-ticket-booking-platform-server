d62f4a28bb02277fe15b29b5c216a71c15901a17
AritraApon <aritromazumdar8@gmail.com>
2026-06-19 12:39:12 +0600

    vendor added there tickets by post methord

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -23,24 +23,41 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-
-
-
-
-
-
-
-
-
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // ---------------------------------------------------------------------
+    // ----------------Database  Collection --------------------------------
+    // ---------------------------------------------------------------------
+    const database = client.db('online-ticket-booking-platform');
+    const ticketsCollection = database.collection('tickets');
+
+
+    // --------------------------------------------------------------------------------------TICKET ROUTES ---------------------------------------------------------------------------------------------------------------
+
+    app.get('/api/tickets', async (req, res) => {
+      const cursor = ticketsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // ----------------------TICKET-POST-----------------------
+    app.post('/api/tickets', async (req, res) => {
+      const newTicket = req.body;
+      const result = await ticketsCollection.insertOne(newTicket);
+      res.send(result);
+    });
+
+
+    // --------------------------------------------------------------------------------------BOOKING ROUTES ---------------------------------------------------------------------------------------------------------------
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 
